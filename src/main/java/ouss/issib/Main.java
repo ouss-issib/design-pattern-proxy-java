5a3e705585a542bc51e97b59fddced2278bc8e1c
ouss-issib <oussamaissib@gmail.com>
2025-11-08 14:09:56 +0100

    feat : implementation code by using the  proxycache

--- src/main/java/ouss/issib/Main.java
+++ src/main/java/ouss/issib/Main.java
@@ -1,14 +1,19 @@
 package ouss.issib;
 
 import ouss.issib.controllers.Client;
+import ouss.issib.proxy.ProxyCache;
 import ouss.issib.services.IService;
 import ouss.issib.services.ServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        IService service = new ServiceImpl();
+//        IService service = new ServiceImpl();
+        IService service = new ProxyCache();
         Client client = new Client();
         client.setService(service);
+        System.out.println("Result = " + client.perform(45));
+        System.out.println("Result = " + client.perform(45));
+        System.out.println("Result = " + client.perform(45));
         System.out.println("Result = " + client.perform(45));
     }
 }
--- src/main/java/ouss/issib/services/ServiceImpl.java
+++ src/main/java/ouss/issib/services/ServiceImpl.java
@@ -3,6 +3,7 @@ package ouss.issib.services;
 public class ServiceImpl implements IService{
     @Override
     public double compute(int t) {
+        System.out.println("Computing .....");
         return Math.cos(t * Math.PI/180 + Math.sqrt(t));
     }
 }
